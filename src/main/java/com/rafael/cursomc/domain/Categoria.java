5d9006899269720d7e00a2f80a232c4a8e080d96
Rafael Monteiro <rafaelmonteir92@gmail.com>
2018-09-29 19:42:23 -0300

    testanto primeira classe de dominio

---
+++ src/main/java/com/rafael/cursomc/domain/Categoria.java
@@ -0,0 +1,69 @@
+package com.rafael.cursomc.domain;
+
+import java.io.Serializable;
+
+public class Categoria implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Integer Id;
+	private String Nome;
+	
+	public Categoria() {
+		
+	}
+
+	public Categoria(Integer id, String nome) {
+		super();
+		setId(id);
+		setNome(nome);
+	}
+
+	public Integer getId() {
+		return Id;
+	}
+
+	public void setId(Integer id) {
+		Id = id;
+	}
+
+	public String getNome() {
+		return Nome;
+	}
+
+	public void setNome(String nome) {
+		Nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((Id == null) ? 0 : Id.hashCode());
+		result = prime * result + ((Nome == null) ? 0 : Nome.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		if (Id == null) {
+			if (other.Id != null)
+				return false;
+		} else if (!Id.equals(other.Id))
+			return false;
+		if (Nome == null) {
+			if (other.Nome != null)
+				return false;
+		} else if (!Nome.equals(other.Nome))
+			return false;
+		return true;
+	}
+	
+	
+}
