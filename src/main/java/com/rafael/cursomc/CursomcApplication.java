ade20ded99f2e68bd579a41512111c00e9db662e
Rafael Monteiro <rafaelmonteir92@gmail.com>
2018-10-28 22:41:57 -0300

    Estados e cidades

--- src/main/java/com/rafael/cursomc/CursomcApplication.java
+++ src/main/java/com/rafael/cursomc/CursomcApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.rafael.cursomc.domain.Categoria;
+import com.rafael.cursomc.domain.Cidade;
+import com.rafael.cursomc.domain.Estado;
 import com.rafael.cursomc.domain.Produto;
 import com.rafael.cursomc.repositories.CategoriaRepository;
+import com.rafael.cursomc.repositories.CidadeRepository;
+import com.rafael.cursomc.repositories.EstadoRepository;
 import com.rafael.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -19,6 +23,10 @@ public class CursomcApplication implements CommandLineRunner{
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	@Autowired
+	private EstadoRepository estadoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -40,8 +48,22 @@ public class CursomcApplication implements CommandLineRunner{
 		p1.getCategorias().add(cat1);
 		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
 		p3.getCategorias().add(cat1);
-		
+				
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "Sao Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlandia", est1);
+		Cidade c2 = new Cidade(null, "Sao Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2,c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1,est2));
+		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
+		
 	}
 }
