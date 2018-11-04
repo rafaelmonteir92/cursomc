ad063613908b9d98d2504548663d7abed0240d80
Rafael Monteiro <rafaelmonteir92@gmail.com>
2018-11-04 21:17:26 -0200

    clientes/{id} disponivel

--- src/main/java/com/rafael/cursomc/resources/ClienteResource.java
+++ src/main/java/com/rafael/cursomc/resources/ClienteResource.java
@@ -1,5 +1,8 @@
 package com.rafael.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,20 +10,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.rafael.cursomc.domain.Categoria;
-import com.rafael.cursomc.services.CategoriaService;
+import com.rafael.cursomc.domain.Cliente;
+import com.rafael.cursomc.services.ClienteService;
 
 @RestController
-@RequestMapping(value="/categorias")
-public class CategoriaResource {
+@RequestMapping(value="clientes")
+public class ClienteResource {
 
 	@Autowired
-	private CategoriaService service;
+	private ClienteService service;
 	
 	@RequestMapping(value="/{id}",method=RequestMethod.GET)
 	public ResponseEntity<?> Find(@PathVariable Integer id) {
 		
-		Categoria obj = service.buscar(id);			
+		Cliente obj = service.buscar(id);			
 		
 		return ResponseEntity.ok(obj);
 	}
