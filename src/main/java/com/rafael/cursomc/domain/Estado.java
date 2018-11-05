3b0260f7cc2d9417c0b87d3e0529aa94f4a0f4fd
Rafael Monteiro <rafaelmonteir92@gmail.com>
2018-11-04 22:10:54 -0200

    Pedido Pagamento EstadoPagamento

--- src/main/java/com/rafael/cursomc/domain/Estado.java
+++ src/main/java/com/rafael/cursomc/domain/Estado.java
@@ -11,7 +11,7 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 
 @Entity
 public class Estado implements Serializable {
