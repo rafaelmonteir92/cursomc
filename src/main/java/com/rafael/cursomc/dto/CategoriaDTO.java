76fb888bb919b70b75d96d0f14765ed8b7690557
Rafael Monteiro <rafaelmonteir92@gmail.com>
2018-12-09 20:12:29 -0200

    validacao sinntatica com Bean validation

--- src/main/java/com/rafael/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/rafael/cursomc/dto/CategoriaDTO.java
@@ -2,15 +2,17 @@ package com.rafael.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+import org.hibernate.validator.constraints.Length;
+
 import com.rafael.cursomc.domain.Categoria;
 
-public class CategoriaDTO implements Serializable{
-
-	/**
-	 * 
-	 */
+public class CategoriaDTO implements Serializable{	
 	private static final long serialVersionUID = 1L;
+	
 	private Integer Id;
+	@NotEmpty(message="Preenchimento obrigatorio")
+	@Length(message="O nome de ter de 5 a 80 caracteres")
 	private String Nome;
 	
 	public CategoriaDTO() {
--- src/main/java/com/rafael/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/rafael/cursomc/resources/CategoriaResource.java
@@ -4,6 +4,8 @@ import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
@@ -35,7 +37,8 @@ public class CategoriaResource {
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)	
-	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+	public ResponseEntity<Void> insert(@Valid @RequestBody CategoriaDTO objDTO){
+		Categoria obj = service.fromDTO(objDTO);
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
